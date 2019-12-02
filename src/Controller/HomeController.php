38bee274a76b91aca84121cbe7df5c390280e88a
AngelRionCervi <a.rioncervi@gmail.com>
2019-12-02 18:15:21 +0100

    added backend

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -45,9 +48,47 @@ class HomeController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function contactAction()
+    public function contactAction(Request $request, \Swift_Mailer $mailer)
     {
 
-        return $this->render('contact.html.twig');
+        $defaultData = [];
+        $formContact = $this->createFormBuilder($defaultData)
+            ->add('email', EmailType::class, [
+                    'attr' => [
+                        'placeholder' => 'Email',
+                    ]
+            ])
+            ->add('message', TextareaType::class, [
+                'attr' => [
+                    'placeholder' => 'Message',
+                ]
+            ])
+            ->getForm();
+
+        $formContact->handleRequest($request);
+
+        if ($formContact->isSubmitted() && $formContact->isValid()) {
+            $data = $formContact->getData();
+            $message = (new \Swift_Message())
+                ->setFrom('a.rioncervi@gmail.com')
+                ->setTo('a.rioncervi@gmail.com')
+                ->setSubject('someone wrote something from you portfolio you should open this now')
+                ->setBody(
+                    "from: " . $data['email'] . "\n\n" . $data['message']
+            );
+
+            $mailer->send($message);
+
+            return $this->render('contact.html.twig', [
+                'form' => $formContact->createView(),
+                'messaged' => true
+            ]);
+        }
+
+
+        return $this->render('contact.html.twig', [
+            'form' => $formContact->createView(),
+            'messaged' => false
+        ]);
     }
 }
