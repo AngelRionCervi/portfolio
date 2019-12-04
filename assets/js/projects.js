a48b90e372c9ff9e886a4cddf20073507542a058
AngelRionCervi <a.rioncervi@gmail.com>
2019-12-04 14:36:36 +0100

    working on js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -12,14 +12,25 @@ function initProject() {
     let idShow = document.querySelector('.title-project-container > p');
     let descriptionShow = document.querySelector('.project-description > p');
     let imageShow = document.querySelector('.image-nav-container > img');
-    let specShow = document.querySelectorAll('.skill-used > .icon-skill');
+    let specShow = Array.from(document.querySelectorAll('.skill-used > .icon-skill'));
 
-    console.log(imageShow);
     let defaultProject = projects[0];
 
     titleShow.textContent = defaultProject.querySelector('.hid-p-title').textContent.trim();
     idShow.textContent = defaultProject.querySelector('.hid-p-id').textContent.trim() + '.';
     descriptionShow.textContent = defaultProject.querySelector('.hid-p-desc').textContent.trim();
+    imageShow.src = 'images/' + defaultProject.querySelectorAll('.hid-p-image')[0].innerText.trim();
+    let projectSpecs = Array.from(defaultProject.getElementsByClassName('hid-p-spec'));
+
+    specShow.forEach(v => {
+        v.classList.add('display-none');
+    });
+
+
+    for (let i = 0; i < projectSpecs.length; i++) {
+        specShow[i].classList.remove('display-none');
+        specShow[i].src = 'images/specs/' + projectSpecs[i].innerText.trim();
+    }
 
 
 }
