cc0730e5ea9553142dba205e9dd2088b4cbdda66
AngelRionCervi <a.rioncervi@gmail.com>
2019-12-04 18:08:46 +0100

    added transitions

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -15,10 +15,19 @@ function initProject(index) {
 
     let project = projects[index];
 
-    titleShow.textContent = project.querySelector('.hid-p-title').textContent.trim();
-    idShow.textContent = project.querySelector('.hid-p-id').textContent.trim() + '.';
-    descriptionShow.textContent = project.querySelector('.hid-p-desc').textContent.trim();
+    opacityZero(titleShow, idShow, descriptionShow);
+
+    setTimeout(() => {
+        opacityOne(titleShow, idShow, descriptionShow);
+
+        titleShow.textContent = project.querySelector('.hid-p-title').textContent.trim();
+        idShow.textContent = project.querySelector('.hid-p-id').textContent.trim() + '.';
+        descriptionShow.textContent = project.querySelector('.hid-p-desc').textContent.trim();
+
+    }, 300);
+
     imageShow.src = 'images/' + project.querySelectorAll('.hid-p-image')[0].innerText.trim();
+
     let projectSpecs = Array.from(project.getElementsByClassName('hid-p-spec'));
 
     specShow.forEach(v => {
@@ -33,13 +42,28 @@ function initProject(index) {
     handleCarousel(project);
 }
 
+function opacityZero(titleShow, idShow, descriptionShow) {
+    titleShow.style.opacity = 0;
+    idShow.style.opacity = 0;
+    descriptionShow.style.opacity = 0;
+}
+
+function opacityOne(titleShow, idShow, descriptionShow) {
+    titleShow.style.opacity = 1;
+    idShow.style.opacity = 1;
+    descriptionShow.style.opacity = 1;
+}
+
 function handleSwitch() {
     let projectsButton = Array.from(document.querySelectorAll('.bar, .project-bar'));
+
+    projectsButton[0].classList.add('bar-active');
+    projectsButton[0].querySelector('p').style.backgroundPosition = "unset";
 
     projectsButton.forEach((button, index) => {
         button.addEventListener('click', () => {
             projectsButton.forEach(disableButton => {
-                if(disableButton.classList.contains('bar-active')) {
+                if (disableButton.classList.contains('bar-active')) {
                     disableButton.classList.remove('bar-active');
                     disableButton.querySelector('p').style.backgroundPosition = "";
                 }
@@ -62,16 +86,21 @@ function handleCarousel(project) {
 
     buttonFwd.addEventListener('click', () => {
         imageIndex++;
-        changeImage()
+        changeImage('fwd')
     });
 
     buttonBack.addEventListener('click', () => {
         imageIndex--;
-        changeImage()
+        changeImage('back')
     });
 
-    function changeImage() {
-        cover.classList.add('animate-cover');
+    function changeImage(direction) {
+        if (direction === 'fwd') {
+            cover.classList.add('animate-cover-right');
+        } else {
+            cover.classList.add('animate-cover-left');
+        }
+
         setTimeout(() => {
             if (imageIndex < 0) {
                 imageIndex = projectImages.length - 1;
@@ -82,8 +111,12 @@ function handleCarousel(project) {
             imageShow.src = 'images/' + projectImages[imageIndex].textContent.trim();
         }, 250);
 
-        setTimeout(()=>{
-            cover.classList.remove('animate-cover');
+        setTimeout(() => {
+            if (direction === 'fwd') {
+                cover.classList.remove('animate-cover-right');
+            } else {
+                cover.classList.remove('animate-cover-left');
+            }
         }, 600)
     }
 }
